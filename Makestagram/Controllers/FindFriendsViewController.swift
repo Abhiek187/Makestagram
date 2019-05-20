ce3ed54c8cfa0bfa05bd9be662888ce99014d68c
Abhishek Chaudhuri <achaudhuri2011@yahoo.com>
2019-05-20 16:12:53 -0400

    Add tab bar items

---
+++ Makestagram/Controllers/FindFriendsViewController.swift
@@ -0,0 +1,14 @@
+//
+//  FindFriendsViewController.swift
+//  Makestagram
+//
+//  Created by Basanta Chaudhuri on 5/20/19.
+//  Copyright © 2019 Make School. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class FindFriendsViewController: UIViewController {
+    
+}
---
+++ Makestagram/Controllers/HomeViewController.swift
@@ -0,0 +1,14 @@
+//
+//  HomeViewController.swift
+//  Makestagram
+//
+//  Created by Basanta Chaudhuri on 5/20/19.
+//  Copyright © 2019 Make School. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class HomeViewController: UIViewController {
+    
+}
