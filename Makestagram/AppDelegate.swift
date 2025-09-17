5531c20a4bf26f67eb53e8d3118b00166889de17
Abhiek187 <achaudhuri2011@yahoo.com>
2025-09-16 22:30:31 -0400

    Upgrade to Xcode 26

--- Makestagram/AppDelegate.swift
+++ Makestagram/AppDelegate.swift
@@ -28,8 +28,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             didFinishLaunchingWithOptions: launchOptions
         )
         
-        configureInitialRootViewController(for: window)
-        
         return true
     }
     
@@ -71,26 +69,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
-
-
 }
-
-extension AppDelegate {
-    func configureInitialRootViewController(for window: UIWindow?) {
-        let defaults = UserDefaults.standard
-        let initialViewController: UIViewController
-        
-        if let _ = Auth.auth().currentUser,
-            let userData = defaults.object(forKey: Constants.UserDefaults.currentUser) as? Data,
-            let user = try? JSONDecoder().decode(User.self, from: userData) {
-            User.setCurrent(user)
-            initialViewController = UIStoryboard.initialViewController(for: .main)
-        } else {
-            initialViewController = UIStoryboard.initialViewController(for: .login)
-        }
-        
-        window?.rootViewController = initialViewController
-        window?.makeKeyAndVisible()
-    }
-}
-
---
+++ Makestagram/SceneDelegate.swift
@@ -0,0 +1,82 @@
+//
+//  SceneDelegate.swift
+//  Makestagram
+//
+//  Created by Abhishek Chaudhuri on 9/16/25.
+//  Copyright © 2025 Make School. All rights reserved.
+//
+
+import UIKit
+import FirebaseAuth
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(
+        _ scene: UIScene,
+        willConnectTo session: UISceneSession,
+        options connectionOptions: UIScene.ConnectionOptions
+    ) {
+        // Use this method to optionally configure and attach the UIWindow `window`
+        // to the provided UIWindowScene `scene`.
+        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
+        // This delegate does not imply the connecting scene or session are new
+        // (see `application:configurationForConnectingSceneSession` instead).
+        guard let windowScene = scene as? UIWindowScene else { return }
+        
+        configureInitialRootViewController(for: windowScene)
+        
+        
+    }
+
+    func sceneDidDisconnect(_ scene: UIScene) {
+        // Called as the scene is being released by the system.
+        // This occurs shortly after the scene enters the background, or when its session is discarded.
+        // Release any resources associated with this scene that can be re-created the next time the scene connects.
+        // The scene may re-connect later, as its session was not necessarily discarded
+        // (see `application:didDiscardSceneSessions` instead).
+    }
+
+    func sceneDidBecomeActive(_ scene: UIScene) {
+        // Called when the scene has moved from an inactive state to an active state.
+        // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
+    }
+
+    func sceneWillResignActive(_ scene: UIScene) {
+        // Called when the scene will move from an active state to an inactive state.
+        // This may occur due to temporary interruptions (ex. an incoming phone call).
+    }
+
+    func sceneWillEnterForeground(_ scene: UIScene) {
+        // Called as the scene transitions from the background to the foreground.
+        // Use this method to undo the changes made on entering the background.
+    }
+
+    func sceneDidEnterBackground(_ scene: UIScene) {
+        // Called as the scene transitions from the foreground to the background.
+        // Use this method to save data, release shared resources, and store enough scene-specific state information
+        // to restore the scene back to its current state.
+    }
+}
+
+extension SceneDelegate {
+    func configureInitialRootViewController(for scene: UIWindowScene) {
+        let defaults = UserDefaults.standard
+        let initialViewController: UIViewController
+        
+        if let _ = Auth.auth().currentUser,
+            let userData = defaults.object(forKey: Constants.UserDefaults.currentUser) as? Data,
+            let user = try? JSONDecoder().decode(User.self, from: userData) {
+            User.setCurrent(user)
+            initialViewController = UIStoryboard.initialViewController(for: .main)
+        } else {
+            initialViewController = UIStoryboard.initialViewController(for: .login)
+        }
+        
+        window = UIWindow(windowScene: scene)
+        window?.rootViewController = initialViewController
+        window?.makeKeyAndVisible()
+    }
+}
+
